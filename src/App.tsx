dc0f94465f7694064a5c719c0c0558d6de6cf9a3
zeeshanghazanfar97 <77689220+zeeshanghazanfar97@users.noreply.github.com>
2025-06-04 12:35:10 +0500

    Add missing useState import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import ChatInterface from './components/ChatInterface';
