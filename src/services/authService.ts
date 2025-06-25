57fdc915eb03f816d988a2b5e3d2eb990ed8e8d3
zeeshanghazanfar97 <77689220+zeeshanghazanfar97@users.noreply.github.com>
2025-06-25 16:23:41 +0500

    Updated authService.ts

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://api.girlzlovetech.org';
+const API_BASE_URL = 'https://glt-backend.glt-sandbox.com';
 
 export type UserType = 'student' | 'teacher' | 'parent' | 'community_member' | 'sponsor';
 
